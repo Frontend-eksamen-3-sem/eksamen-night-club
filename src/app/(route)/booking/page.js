76ab108b0cc52477e77758cfc23f1731d1b0ff2e
Heleneomdahl <Heom0001@stud.kea.dk>
2025-12-02 15:49:59 +0100

    Oprettelse og implementering af Title.jsx på de adskellige page.js

---
+++ src/app/(route)/booking/page.js
@@ -0,0 +1,15 @@
+import Header from "../../components/Header";
+import Title from "../../components/Title";
+import Footer from "../../components/Footer";
+
+export default function Booking() {
+  return (
+    <div className="">
+      <main className="">
+        <Header />
+        <Title>BOOK TABLE</Title>
+        <Footer />
+      </main>
+    </div>
+  );
+}
---
+++ src/app/(route)/contact/page.js
@@ -0,0 +1,15 @@
+import Header from "../../components/Header";
+import Title from "../../components/Title";
+import Footer from "../../components/Footer";
+
+export default function Contact() {
+  return (
+    <div className="">
+      <main className="">
+        <Header />
+        <Title>CONTACT US</Title>
+        <Footer />
+      </main>
+    </div>
+  );
+}
