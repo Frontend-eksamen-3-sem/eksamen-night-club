4a1e9e2df65e6d91098d262e043f1394b2f1cf6d
Heleneomdahl <Heom0001@stud.kea.dk>
2025-12-02 15:52:26 +0100

    Udkommentering af kode i index (page.js) for bedre overblik i opstartsfasen over anvendte komponenter

--- src/app/page.js
+++ src/app/page.js
@@ -1,29 +1,29 @@
-import IndexHero from "./components/index/IndexHero";
+// import IndexHero from "./components/index/IndexHero";
 import Header from "./components/Header";
-import Welcome from "./components/index/Welcome";
-import EventMonth from "./components/index/EventMonth";
-import Gallery from "./components/index/Gallery";
-import MusicCarousel from "./components/index/MusicCarousel";
-import VideoCarousel from "./components/index/VideoCarousel";
-import Testimonials from "./components/index/Testimonials";
-import RecentBlog from "./components/index/RecentBlog";
-import EmailForm from "./components/index/EmailForm";
+// import Welcome from "./components/index/Welcome";
+// import EventMonth from "./components/index/EventMonth";
+// import Gallery from "./components/index/Gallery";
+// import MusicCarousel from "./components/index/MusicCarousel";
+// import VideoCarousel from "./components/index/VideoCarousel";
+// import Testimonials from "./components/index/Testimonials";
+// import RecentBlog from "./components/index/RecentBlog";
+// import EmailForm from "./components/index/EmailForm";
 import Footer from "./components/Footer";
 
 export default function Home() {
   return (
     <div className="">
       <main className="">
-        <IndexHero />
+        {/* <IndexHero /> */}
         <Header />
-        <Welcome />
+        {/* <Welcome />
         <EventMonth />
         <Gallery />
         <MusicCarousel />
         <VideoCarousel />
         <Testimonials />
         <RecentBlog />
-        <EmailForm />
+        <EmailForm /> */}
         <Footer />
       </main>
     </div>
