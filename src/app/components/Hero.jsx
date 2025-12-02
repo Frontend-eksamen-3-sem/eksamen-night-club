581308a84d00d8a9e4077ae7a122adbf267f882b
Heleneomdahl <Heom0001@stud.kea.dk>
2025-12-02 16:13:16 +0100

    Overførsel af komponent-indhold fra Title-Hero

---
+++ src/app/components/Hero.jsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import BottomLine from "../../../assets/bottom_line.png";
+
+const Hero = ({ children, className = "" }) => {
+  return (
+    <div className={`w-full ${className}`}>
+      <h1 className="w-full text-center">{children}</h1>
+      <Image src={BottomLine} alt="title underline img" className="mx-auto mt-3" />
+    </div>
+  );
+};
+
+export default Hero;
