82ac7397eab685ea3685c97272573840c0132a2d
Heleneomdahl <Heom0001@stud.kea.dk>
2025-12-05 11:50:26 +0100

    Ændring i page.js layout

--- src/app/(route)/booking/page.js
+++ src/app/(route)/booking/page.js
@@ -4,16 +4,28 @@ import TableGrid from "./components/TableGrid";
 import Button from "../../components/Button";
 import Footer from "../../components/Footer";
 
+import Image from "next/image";
+import Footerbg from "@/assets/bg/footerbg.jpg";
+
 export default function Booking() {
   return (
-    <div className="">
-      <main className="">
+    <>
+      <header className="grid grid-cols-subgrid col-[full] ">
+        {/* <IndexHero /> */}
+
         <Header />
+      </header>
+      <main className="grid col-[content]">
         <Hero>BOOK TABLE</Hero>
         <Button>RESERVE</Button>
         <TableGrid />
-        <Footer />
       </main>
-    </div>
+      <footer className="grid col-[full] grid-cols-subgrid">
+        <Image src={Footerbg} alt="Footerbg" className="col-span-full row-span-full w-full h-full object-cover z-0 opacity-10 " />
+        <div className="grid col-[content] row-start-1">
+          <Footer />
+        </div>
+      </footer>
+    </>
   );
 }
