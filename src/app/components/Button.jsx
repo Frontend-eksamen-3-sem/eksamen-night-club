3eedf71aa0f09be4daba19449975c7a5ab5ddd4f
Heleneomdahl <Heom0001@stud.kea.dk>
2025-12-09 15:59:36 +0100

    Tilpasning af vores button

--- src/app/components/Button.jsx
+++ src/app/components/Button.jsx
@@ -1,7 +1,7 @@
 const Button = ({ children, className = "" }) => {
   return (
-    <div className={`border-t border-b border-white text-text  ${className}`}>
-      <button className="w-full text-center grid content-center">{children}</button>
+    <div className={`border-t border-b border-white max-w-25   ${className}`}>
+      <button className="custom-btn">{children}</button>
     </div>
   );
 };
