934a5f52909b035eb3b38ccdf87f9acc47897f43
lakr0003 <lakr0003@stud.kea.dk>
2025-12-09 18:37:15 +0100

    dynamisk layout og pile

--- src/app/components/index/Gallery.jsx
+++ src/app/components/index/Gallery.jsx
@@ -24,26 +24,9 @@ const FetchGallery = async () => {
 
   const maxseven = image.slice(0, 7);
 
-  // Grid-positions som i dit statiske eksempel
-  // const positions = [
-  //   { colStart: 1, colEnd: 3, rowSpan: 1 },
-  //   { colStart: 3, colEnd: 4, rowSpan: 1 },
-  //   { colStart: 4, colEnd: 6, rowSpan: 1 },
-  //   { colStart: 6, colEnd: 7, rowSpan: 1 },
-  //   { colStart: 1, colEnd: 3, rowSpan: 2 },
-  //   { colStart: 3, colEnd: 5, rowSpan: 2 },
-  //   { colStart: 5, colEnd: 7, rowSpan: 2 },
-  // ];
-
   return (
     <>
       <GalleryClient image={maxseven} />;
-      {/* <div className="grid grid-cols-6 grid-rows-2 items-stretch">
-        {maxseven.map((image, index) => {
-          const pos = positions[index] || { colStart: 1, colEnd: 2, rowSpan: 1 };
-          return <Image key={image.id} src={image.asset?.url} alt={image.description} className={`col-start-${pos.colStart} col-end-${pos.colEnd} row-span-${pos.rowSpan} max-h-[250] max-w-[250px] object-cover`} width={500} height={500} unoptimized />;
-        })}
-      </div> */}
     </>
   );
 };
