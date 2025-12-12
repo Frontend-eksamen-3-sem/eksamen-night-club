90ad2973f21a66bf764f0d0cb3888f385a8e91e3
klhe0002 <klhe0002@stud.kea.dk>
2025-12-12 23:21:15 +0100

    import af npm pakke

--- src/app/components/index/MusicCarousel.jsx
+++ src/app/components/index/MusicCarousel.jsx
@@ -1,7 +1,23 @@
+"use client";
 import Title from "../Title";
+import AudioPlayer from "react-h5-audio-player";
+import "react-h5-audio-player/lib/styles.css";
+const AudioOne = "/assets/media/black-box-funky.mp3";
 
 const MusicCarousel = () => {
-  return <Title>NIGHT CLUB Track</Title>;
+  return (
+    <section className="grid col-[content] pt-10">
+      <div>
+        <Title>NIGHT CLUB TRACK</Title>
+        <AudioPlayer
+          autoPlay
+          src={AudioOne}
+          onPlay={(e) => console.log("onPlay")}
+          // other props here
+        />
+      </div>
+    </section>
+  );
 };
 
 export default MusicCarousel;
