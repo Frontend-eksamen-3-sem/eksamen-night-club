ce6c0e680d2ad70d1b16fb3b9a6ca009be0155ac
Heleneomdahl <Heom0001@stud.kea.dk>
2025-12-12 14:24:23 +0100

    margin tilføjet på Title.jsx

--- src/app/components/Title.jsx
+++ src/app/components/Title.jsx
@@ -2,9 +2,11 @@ import Image from "next/image";
 import BottomLine from "@/assets/bottom_line.png";
 const Title = ({ children, className = "" }) => {
   return (
-    <div className={`col-[content] w-full max-w-md mx-auto flex flex-col items-center ${className}`}>
-      <h1 className="w-fit text-center">{children}</h1>
-      <Image src={BottomLine} alt="title underline img" className="w-full" />
+    <div className={`col-[content] w-full max-w-md mx-auto flex flex-col items-center mt-14 mb-8 ${className}`}>
+      <h1 className="w-full text-center">{children}</h1>
+      <div className="w-full max-w-50 mx-auto mt-3">
+        <Image src={BottomLine} alt="title underline img" className="w-full" />
+      </div>
     </div>
   );
 };
