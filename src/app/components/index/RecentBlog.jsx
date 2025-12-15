8ab8a4529e3cc38661fed091d54e770d20d5edce
lakr0003 <lakr0003@stud.kea.dk>
2025-12-15 14:19:16 +0100

    recentblogposts til indexside

--- src/app/components/index/RecentBlog.jsx
+++ src/app/components/index/RecentBlog.jsx
@@ -1,7 +1,31 @@
+import { Suspense } from "react";
+import RecentBlogCard from "./RecentBlogCard";
 import Title from "../Title";
 
 const RecentBlog = () => {
-  return <Title>RECENT BLOG</Title>;
+  return (
+    <>
+      <Title>RECENT BLOG</Title>
+      <Suspense fallback={<div className="p-4">Loading blog…</div>}>
+        <FetchBlog />
+      </Suspense>
+    </>
+  );
 };
 
 export default RecentBlog;
+
+const FetchBlog = async () => {
+  "use server";
+  const response = await fetch(`http://localhost:4000/blogposts/?embed=comments`, { cache: "no-store" });
+  const blog = await response.json();
+  console.log(blog);
+
+  const posts = blog.slice(0, 3);
+
+  return (
+    <>
+      <RecentBlogCard blog={posts} />;
+    </>
+  );
+};
