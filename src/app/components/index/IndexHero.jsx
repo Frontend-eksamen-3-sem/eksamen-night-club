62b4647537873cf0f3228f770465be0abbd2b423
lakr0003 <lakr0003@stud.kea.dk>
2025-12-16 21:07:50 +0100

    tilføjet loading eager

--- src/app/components/index/IndexHero.jsx
+++ src/app/components/index/IndexHero.jsx
@@ -14,7 +14,7 @@ const IndexHero = () => {
     <section className="col-[full]">
       <div className="grid grid-cols-3">
         <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 0.5 }} className="col-start-1 row-start-1 col-end-4 row-end-4 h-[800px] md:max-h-[600px] md:w-full md:overflow-hidden">
-          <Image src={randomImg} alt="Header" className="w-full h-full object-cover object-center" />
+          <Image src={randomImg} alt="Header" className="w-full h-full object-cover object-center" loading="eager" />
         </motion.div>
         <div className="col-start-1 col-end-4 row-start-2 row-end-3 md:max-w-[900px] mx-auto">
           <motion.div initial={{ opacity: 0, rotateX: 90 }} animate={{ opacity: 1, rotateX: 0 }} transition={{ duration: 1, ease: "easeIn" }} className="mx-auto w-full" style={{ perspective: 1000 }}>
