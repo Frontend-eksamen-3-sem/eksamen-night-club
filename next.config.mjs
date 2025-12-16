f0d0f7235a9c9a82709e152c03312a598faf3271
klhe0002 <klhe0002@stud.kea.dk>
2025-12-16 22:15:23 +0100

    mere ren config

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,18 @@
-// ...existing code...
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   cacheComponents: true,
   images: {
     remotePatterns: [
-      { protocol: "https", hostname: "cdn.dummyjson.com", pathname: "/**" },
-      { protocol: "http", hostname: "localhost", port: "4000", pathname: "/**" },
-      { protocol: "http", hostname: "127.0.0.1", port: "4000", pathname: "/**" },
-      // tilføj uden port i tilfælde hvor port ikke matches
-      { protocol: "http", hostname: "localhost", pathname: "/**" },
-      { protocol: "http", hostname: "127.0.0.1", pathname: "/**" },
+      {
+        protocol: "http",
+        hostname: "localhost",
+        port: "4000",
+        pathname: "/**",
+      },
     ],
   },
 };
 
 export default nextConfig;
-// ...existing code...
