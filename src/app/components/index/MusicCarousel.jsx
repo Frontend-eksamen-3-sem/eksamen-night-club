e7c62b73e615c8189b98057464eb625de83cf024
lakr0003 <lakr0003@stud.kea.dk>
2025-12-16 21:10:32 +0100

    rettet carrouselpile

--- src/app/components/index/MusicCarousel.jsx
+++ src/app/components/index/MusicCarousel.jsx
@@ -117,36 +117,9 @@ const MusicCarousel = () => {
           </CarouselContent>
 
           {artists.length > 5 && (
-            <div className="grid grid-cols-2 justify-items-center md:block">
-              <CarouselPrevious
-                className=" 
-        static!
-        transform-none!
-        translate-y-0!
-        top-auto!
-        left-auto!
-        md:absolute!
-        md:top-1/2!
-        md:-left-12!
-        md:-translate-y-1/2!
-        border-white! text-white! 
-      "
-              />
-
-              <CarouselNext
-                className="
-        static! 
-        transform-none!
-        translate-y-0!
-        top-auto!
-        left-auto!
-        md:absolute!
-        md:top-1/2!
-        md:-left-12!
-        md:-translate-y-1/2!
-        border-white! text-white!
-      "
-              />
+            <div className="mt-4 flex justify-center gap-4">
+              <CarouselPrevious className="z-50 static translate-y-0! text-text hover:text-accent" />
+              <CarouselNext className="z-50 static translate-y-0! text-text hover:text-accent" />
             </div>
           )}
         </Carousel>
