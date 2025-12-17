ecd83b01a141dd827831730ebf44eb5ba9476ba2
Heleneomdahl <Heom0001@stud.kea.dk>
2025-12-17 16:29:14 +0100

    Ændring af komponenter i IndexHero-komponenterne for at undgå hydration-fejl, bl.a. ved import af connection

--- src/app/components/index/IndexHero.jsx
+++ src/app/components/index/IndexHero.jsx
@@ -1,16 +1,10 @@
 import { Suspense } from "react";
-import headerImg1 from "@/assets/bg/header_bg_1.jpg";
-import headerImg2 from "@/assets/bg/header_bg_2.jpg";
-import logoImg from "@/assets/icon/Logo.svg";
-import BottomLine from "@/assets/bottom_line.png";
-import IndexHeroClient from "./IndexHeroClient";
-
-const images = [headerImg1, headerImg2];
+import IndexHeroRandom from "./IndexHeroRandom";
 
 export default function IndexHero() {
   return (
     <Suspense fallback={<div>Loading hero…</div>}>
-      <IndexHeroClient images={images} logoImg={logoImg} BottomLine={BottomLine} />
+      <IndexHeroRandom />
     </Suspense>
   );
 }
--- src/app/components/index/IndexHeroClient.jsx
+++ src/app/components/index/IndexHeroClient.jsx
@@ -1,17 +1,21 @@
 "use client";
-import Image from "next/image";
 import { motion } from "framer-motion";
+import Image from "next/image";
+import headerImg1 from "@/assets/bg/header_bg_1.jpg";
+import headerImg2 from "@/assets/bg/header_bg_2.jpg";
 
-export default function IndexHeroClient({ images, logoImg, BottomLine }) {
-  const randomImg = images[Math.floor(Math.random() * images.length)];
+const images = [headerImg1, headerImg2];
+
+export default function IndexHeroClient({ logoImg, BottomLine, imgIndex }) {
+  const randomImg = images[imgIndex];
 
   return (
     <section className="col-[full]">
       <div className="grid grid-cols-3">
-        <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 0.5 }} className="col-start-1 row-start-1 col-end-4 row-end-4 h-[800px] md:max-h-[600px] md:w-full md:overflow-hidden">
-          <Image src={randomImg} alt="Header" className="w-full h-full object-cover object-center" />
+        <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 0.5 }} className="col-start-1 row-start-1 col-end-4 row-end-4 h-[800px] md:max-h-[600px] relative">
+          <Image src={randomImg} alt="Header" fill className="object-cover object-center" priority />
         </motion.div>
-        <div className="col-start-1 col-end-4 row-start-2 row-end-3 md:max-w-[900px] mx-auto">
+        <div className="col-start-1 col-end-4 row-start-2 row-end-3 md:max-w-[900px] mx-auto z-10">
           <motion.div initial={{ opacity: 0, rotateX: 90 }} animate={{ opacity: 1, rotateX: 0 }} transition={{ duration: 1, ease: "easeIn" }} className="mx-auto w-full" style={{ perspective: 1000 }}>
             <Image src={logoImg} alt="Logo" className="mx-auto w-full" />
           </motion.div>
