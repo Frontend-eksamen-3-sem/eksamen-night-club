619ecc5df16147310b6b24cf60ddc221325bfec9
lakr0003 <lakr0003@stud.kea.dk>
2025-12-17 12:04:43 +0100

    rettet textcomp

--- src/app/(route)/blog/components/TextCom.jsx
+++ src/app/(route)/blog/components/TextCom.jsx
@@ -1,8 +1,14 @@
-const TextComp = ({ title, subtitle, children }) => {
+const TextComp = ({ title, subtitle, children, commentCount, posted }) => {
   return (
     <div>
       <h2 className="pb-1 uppercase">{title}</h2>
-      <h3 className="pb-2 uppercase">{subtitle}</h3>
+      <div className="flex gap-2">
+        <h3>BY: {subtitle}</h3>
+        <span className="text-accent">/</span>
+        <h3>{commentCount} Comments</h3>
+        <span className="text-accent">/</span>
+        <h3>Posted {posted}</h3>
+      </div>
       <p>{children}</p>
     </div>
   );
