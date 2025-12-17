98fc2bb7bd4ca8765d56fd3b2601c512490fcb11
Heleneomdahl <Heom0001@stud.kea.dk>
2025-12-17 21:33:53 +0100

    Tilføjelse af gap på mobilsite i welcome.jsx

--- src/app/components/index/Welcome.jsx
+++ src/app/components/index/Welcome.jsx
@@ -11,7 +11,7 @@ const Welcome = () => {
   return (
     <section className="col-[content] grid grid-cols-subgrid">
       <Title>WELCOME IN NIGHTCLUB</Title>
-      <div className="col-[content] grid md:grid-cols-3 justify-items-center md:justify-items-stretch md:gap-4">
+      <div className="col-[content] grid md:grid-cols-3 justify-items-center md:justify-items-stretch gap-4">
         <WelcomeImageText src={Pic1} alt="welcome picture 1" title="NIGHT CLUB" text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla non rutrum felis. Pell blandit neque. Cras venenatis orci ut massa efficitur, eget pretium nulla blandit. Quisque sodales suscipit convallis. Ut feugiat iaculis est, a feugiat erat." icon={<Image src={SpecialIcon} alt="icon" className="w-8 h-8" />} />
         <WelcomeImageText src={Pic2} alt="welcome picture 2" title="RESTAURANT" text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla non rutrum felis. Pell blandit neque. Cras venenatis orci ut massa efficitur, eget pretium nulla blandit. Quisque sodales suscipit convallis. Ut feugiat iaculis est, a feugiat erat." icon={<PiCallBell className="w-8 h-8" style={{ color: "var(--pink)" }} />} />
         <WelcomeImageText src={Pic3} alt="welcome picture 3" title="BAR" text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla non rutrum felis. Pell blandit neque. Cras venenatis orci ut massa efficitur, eget pretium nulla blandit. Quisque sodales suscipit convallis. Ut feugiat iaculis est, a feugiat erat." icon={<PiCheersLight className="w-8 h-8" style={{ color: "var(--pink)" }} />} />
